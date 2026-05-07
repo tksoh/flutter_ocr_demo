2dac568c170c3f6fddd698b149347b0fe0289322
TK Soh <teekaysoh@gmail.com>
2026-05-07 14:47:36 +0800

    create OCR demo page

---
+++ lib/ocr.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
+
+void doOcr(File imageFile) async {
+  // 1. Create an instance (can specify script like Latin, Chinese, etc.)
+  final textRecognizer = TextRecognizer(script: TextRecognitionScript.latin);
+
+  // 2. Prepare the image (from a file path or camera stream)
+  final inputImage = InputImage.fromFilePath(imageFile.path);
+
+  // 3. Process the image
+  final RecognizedText recognizedText = await textRecognizer.processImage(
+    inputImage,
+  );
+
+  // 4. Extract data
+  String text = recognizedText.text; // The full raw text
+  debugPrint('Found text: $text');
+
+  for (TextBlock block in recognizedText.blocks) {
+    for (TextLine line in block.lines) {
+      debugPrint('Found line: ${line.text}');
+    }
+  }
+
+  // 5. Always close the recognizer when done
+  textRecognizer.close();
+}
