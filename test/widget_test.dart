bfdc42a943d4b831fc163abac8373b9145b41b07
TK Soh <teekaysoh@gmail.com>
2026-05-09 10:28:47 +0800

    unfinished work on app package rename

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:ocr_test/main.dart';
+import 'package:flutter_ocr_demo/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
